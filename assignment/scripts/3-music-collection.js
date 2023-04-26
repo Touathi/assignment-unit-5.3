d8880c2b4ac547b71db3c7701dee7fb14b8679b1
Touathi Vang <Touathi@outlook.com>
2023-04-26 14:00:22 -0500

    added collection array and function-addToCollection, and also returned the objects

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,19 @@
 console.log('***** Music Collection *****')
+
+let Collection = [];
+
+function addToCollection( title1, artist1, yearPublished1 ) {
+    if ( Collection.length < 0 ) {
+        return undefined
+    }
+    else {
+        Collection.push ({
+            title : title1,
+            artist : artist1,
+            yearPublished : yearPublished1
+        });
+        return Collection[Collection.length - 1]
+    }
+}
+console.log(addToCollection('Forever', 'Lewis Capaldi', '2019' ));
+
