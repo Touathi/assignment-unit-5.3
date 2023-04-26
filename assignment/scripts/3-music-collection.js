a3015c5dcb311a10b812711e23e70789ffa41b42
Touathi Vang <Touathi@outlook.com>
2023-04-26 17:49:54 -0500

    added showCollection function and tested it

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,8 +12,27 @@ function addToCollection( title1, artist1, yearPublished1 ) {
             artist : artist1,
             yearPublished : yearPublished1
         });
-        return Collection[Collection.length - 1]
+        return Collection[Collection.length]
     }
 }
-console.log(addToCollection('Forever', 'Lewis Capaldi', '2019' ));
+addToCollection( 'Forever', 'Lewis Capaldi', '2019' );
+addToCollection( 'Hate everything', 'G-soul', '2019' );
+addToCollection( 'What About Now', 'Daughtry', '2006' );
+addToCollection( 'Lips Of An Angel', 'Hinder', '2005' );
+addToCollection( 'Dont Matter', 'Akon', '2006' );
+addToCollection( 'I Wont Give Up', 'Jason Mraz', '2012' );
+addToCollection( 'Lost On You', 'Lewis Capaldi', '2019' )
+
+console.log( 'This is my song collection', Collection);
+
+
+function showCollection(listsongs) {
+   for ( let songs = 0; Collection.length > songs; songs++) {
+        console.log( Collection.length, `songs` );
+}
+    for ( let songs of Collection) {
+        console.log( {title:songs.title}, 'by', {artist:songs.artist}, ', published in', {yearPublished:songs.yearPublished} )
+}
+}
+showCollection(Collection);
 
